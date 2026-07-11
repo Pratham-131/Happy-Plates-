dd2ccb52eb8ff92170f46185c60af770f83db5bc
Pratham Saxena <pratham@example.com>
2026-07-11 14:02:12 +0530

    chore: remove leftover debug/junk files

--- fix_images.js
+++
@@ -1,6 +0,0 @@
-db.foods.find().forEach(function(food) {
-  var encodedName = encodeURIComponent(food.name);
-  var newUrl = "https://placehold.co/400x300/f97316/ffffff?text=" + encodedName;
-  db.foods.updateOne({_id: food._id}, {$set: {imageUrl: newUrl}});
-});
-print("Done updating image URLs");
